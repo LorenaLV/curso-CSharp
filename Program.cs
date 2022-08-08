f1c4c26e1ffd58bcab38216c830760e64cd0d255
LorenaLV <lorenalv93@hotmail.com>
2022-08-08 14:59:18 +0200

    Primeros ejercicios

--- Program.cs
+++ Program.cs
@@ -1,5 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+/*Ejercicio 1
+Escribe un programa que reciba tu nombre y lo escriba por consola.
+*/
 Console.WriteLine("Escribe tu nombre: ");
-string name = Console.ReadLine();
+string name = "Lorena";
 Console.WriteLine("Hola, " + name);
+
+/* Ejercicio 2
+ Escribe un programa que tome la hora y la escriba por pantalla.
+ */
+Console.WriteLine("Dime la hora actual");
+string hora = Console.ReadLine();
+Console.WriteLine("Esta es la hora actual: " + hora);
+
